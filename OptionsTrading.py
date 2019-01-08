2d4dc48b8dc5a6e5e8405680e6c39e483e7088b9
afederici <ajf5@illinois.edu>
2019-01-07 20:18:28 -0600

    quantopian notebook

---
+++ OptionsTrading.py
@@ -0,0 +1,5 @@
+#https://www.nasdaq.com/symbol/qqq/option-chain
+
+#Plan is to strategize option buying with the trend analysis found in the quantopian notebook
+
+#However, as day trading is not typically allowed on robinhood, I will have to extend my one day analysis to two days/
